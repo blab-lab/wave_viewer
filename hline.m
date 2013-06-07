03a884db70fa12b4090ea2eb6be38c105a02bf7c
Carrie Niziolek <carrien@ucsf>
2013-06-07 14:19:21 -0700

    Added horizontal and vertical line f'ns.

---
+++ hline.m
@@ -0,0 +1,11 @@
+function hl = hline(val,color,linestyle)
+% function hl = hline(val,color,linestyle)
+
+a = axis;
+hl = line(a(1:2),val*ones(2,1));
+if nargin >= 2
+  set(hl,'Color',color);
+end
+if nargin >= 3
+  set(hl,'LineStyle',linestyle);
+end
---
+++ vline.m
@@ -0,0 +1,11 @@
+function hl = vline(val,color,linestyle)
+% function hl = vline(val,color,linestyle)
+
+a = axis;
+hl = line(val*ones(2,1),a(3:4));
+if nargin >= 2
+  set(hl,'Color',color);
+end
+if nargin >= 3
+  set(hl,'LineStyle',linestyle);
+end
