18e775302665981b15769f6acaf894408793f1a6
Carrie Niziolek <carrien@bu.edu>
2015-11-06 20:48:58 -0500

    Updated for Matlab new figure behavior

--- moveback.m
+++ moveback.m
@@ -11,7 +11,13 @@ end
 
 axkids = get(hax,'Children');
 n_axkids = length(axkids);
-ih = dsearchn(axkids,h);
+for i=1:n_axkids
+    if axkids(i)==h
+        ih=i;
+        break;
+    end
+end
+
 if isempty(ih), error('handle(%f) not a child of hax(%f)', h, hax); end
 if ih < n_axkids
   new_axkids(1:(ih-1)) = axkids(1:(ih-1));
