552e84904a699e9739f8241249e2ecf799148416
carrien <carrien@bu.edu>
2018-03-19 17:57:11 -0500

    linestyle change

--- hline.m
+++ hline.m
@@ -2,7 +2,7 @@ function hl = hline(val,color,linestyle)
 % function hl = hline(val,color,linestyle)
 
 a = axis;
-hl = line(a(1:2),val*ones(2,1));
+hl = line(a(1:2),val*ones(2,1),'LineWidth',1);
 if nargin >= 2
   set(hl,'Color',color);
 end
