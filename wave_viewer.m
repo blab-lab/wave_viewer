73b4634243cc875591e535207375528ce5b36690
Camden Zhu <123699579+camsinfiniteram@users.noreply.github.com>
2024-11-11 11:03:45 -0600

    wider leftmost panel
    To do: make panel size adjustable?

--- wave_viewer.m
+++ wave_viewer.m
@@ -136,7 +136,7 @@ panelFontSize = .1;
 % create panel for buttons
 buttonPanelXPos = panelPad;
 buttonPanelYPos = panelPad;
-buttonPanelXSpan = 0.1;
+buttonPanelXSpan = 0.2;
 buttonPanelYSpan = 1 - panelPad*2;
 buttonPanelPos = [buttonPanelXPos buttonPanelYPos buttonPanelXSpan buttonPanelYSpan];
 p.guidata.buttonPanel = uipanel(p.guidata.f,'Units','Normalized',...
